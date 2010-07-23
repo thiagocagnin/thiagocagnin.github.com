c3103fbe7a0360cb3fe4f3226819377dafdfbfec
thiagocagnin <thiagocr.utfpr@gmail.com>
2010-07-23 09:43:30 -0300

    Add pagina inicial e pasta da foto

---
+++ index.php
@@ -0,0 +1,302 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+        <title>Thiago Cagnin Rodrigues - Curriculum Vitae</title>
+        <style type="text/css">
+            body {
+                font: 14px arial, verdana, sans-serif;
+                color: #080808;
+                background-color: #f2f2f2;
+            }
+
+            h2{
+                font: 145% Georgia;
+                color: #383838;
+                font-style: italic;
+                border-bottom: 1px solid #a7a6a6;
+            }
+
+            h3{
+                font: 14px arial, verdana, sans-serif;
+                color: #616060;
+                font-style: italic;
+                font-weight: bolder;
+                margin-bottom: 5px;
+            }
+
+            h4{
+                font: 16px arial, verdana, sans-serif;
+                color: #323231;
+                font-style: italic;
+                font-weight: bolder;
+                margin-bottom: 5px;
+            }
+
+            ul{
+                margin: 0;
+            }
+
+            #container{
+                margin: 0 auto;
+                width: 777px;
+            }
+
+            #foto{
+                margin: 0 auto;
+                margin-top: 20px;
+                width: 85px;
+                height: 113px;                
+                background-position: center;
+                background: url(img/foto.jpg) no-repeat;
+            }
+
+
+            #nome_contato{
+                text-align: center;
+                margin-bottom: 40px;
+            }
+
+            #resumo_qualificacoes{
+                margin-bottom: 40px;
+                padding-bottom: 0px;
+            }
+
+            #formacao_academica{
+                line-height: 5px;
+                margin-bottom: 40px;
+            }
+
+            #linguas_estrangeiras{
+                margin-bottom: 40px;
+            }
+
+            #cursos{
+                margin-bottom: 40px;
+            }
+
+            #experiencia_profissional{
+                margin-bottom: 40px;
+            }
+
+            #rodape{
+                margin: 0px;
+                padding: 0px;
+                color: #939392;
+                float: right;
+                margin-bottom: 10px;
+            }
+
+            .contato{
+                font-weight: bolder;
+                font-size: 16px;
+                line-height: 5px;
+            }
+
+            .conhecimento{
+                margin: 0 auto;
+                float: left;
+                width: 50%;
+                margin-top: 0px;
+            }
+
+            .conhecimento ul{
+                height: 60px;
+
+            }
+        </style>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+        <div id="container">
+            <div id="foto">
+            </div>
+            <div id="nome_contato">
+                <h1>
+                    Thiago Cagnin Rodrigues
+                </h1>
+
+                <div class="contato">
+                    <p>21 anos, Solteiro</p>
+                    <p>Rua Pedro Ramirez de Mello, 79, apto. 9</p>
+                    <p>Centro - Pato Branco, PR.</p>
+                    <p>(46) 9914-3794</p>
+                    <p>thiagocr.utfpr@gmail.com</p>
+                </div>
+            </div>
+
+            <div id="resumo_qualificacoes">
+                <h2>
+                    Resumo das Qualifica&ccedil;&otilde;es
+                </h2>
+
+                <ul>
+                    <li>Foco no desenvolvimento de softwares baseados na web e em desktop</li>
+                    <li>Trabalhar efetivamente sozinho ou em equipe</li>
+                    <li>Capacidade em lidar com várias tarefas ao mesmo tempo</li>
+                    <li>Experiência em desenvolvimento Orientado a Objetos</li>
+                    <li>1 ano e 8 meses de experi&ecirc;ncia em Delphi 7 e Delphi 2007. Experiência na criação de relatórios com FastReport e com QuickRepot</li>
+                    <li>2 anos e 8 meses de experi&ecirc;ncia em MSSQL Server 2000/2005</li>
+                    <li>Experi&ecirc;ncia em desenvolvimento com PHP, CSS, HTML, AJAX e JS desde setembro/2009 até atualmente, utilizando o Photoshop para edição e criação de &iacute;cones</li>
+                    <li>Trabalhos paralelos com utilização de MySQL e Postgres</li>
+                    <li>Conhecimento b&aacute;sico em Ruby on Rails. Estrutura, comandos e linguagem</li>
+                    <li>Atualmente, desenvolvendo projeto em ActionScript 3, Flex Builder e PHP para utilização em 2 bancos de dados, PostGres e MSSQL (Independente de Plataforma)</li>
+                    <li>1 ano e 2 meses de experi&ecirc;ncia em suporte técnico e elaboração de manuais</li>
+                    <li>Cursando 6º per&iacute;odo de Análise e Desenvolvimento de Sistema na Universidade Tecnol&oacute;gica Federal do Paran&aacute;. Conclusão prevista para dezembro/2010</li>
+                </ul>
+            </div>
+
+            <div id="experiencia_profissional">
+                <h2>
+                    Experi&ecirc;ncia Profissional
+                </h2>
+
+                <div>
+                    <h4>Desenvolvimento web/desktop</h4>
+                    <p>Xpert (<a href="http://www.xpert.com.br">www.xpert.com.br</a>), Pato Branco, Paran&aacute;. (Janeiro, 2009 - Atualmente)</p>
+                    <ul>
+                        <li><strong>Programa&ccedil;&atilde;o desktop:</strong></li>
+                        <li>Desenvolvimento de aplicativos em Delphi (Aplicativos para exporta&ccedil;&atilde;o e importa&ccedil;&atilde;o de dados)</li>
+                        <li>Manuten&ccedil;&atilde;o e implementa&ccedil;&atilde; do sistema AutoXpert e ATXPDV desenvolvidos em delphi 7 e delphi 2007</li>
+                        <li>Cria&ccedil;&atilde;o e manuten&ccedil;&atilde;o de relat&oacute; utilizando FastReport e QuickReport</li>
+                        <li>Cria&ccedil;&atilde;o, manuten&ccedil;&atilde;o e implementa&ccedil;&atilde; de c&oacute;digos em linguagem TSql (SQL Server)</li>
+                        <li>Desenvolvimento de procedures, trigguers e views</li>
+                        <li>Utilização do Microsoft Visual Source Safe para o compartilhamento de c&oacute;digo e controle de vers&atilde;o</li>
+                        
+                        <br /><li><strong>Programa&ccedil;&atilde;o web:</strong></li>
+                        <li>Desenvolvimento do site da empresa</li>
+                        <li>Utiliza&AElig;&atilde;o de PHP, MySql, Hml, Xml, JavaScript e Ajax para o desenvolvimento do site</li>
+                        <li>Migra&ccedil;&atilde;o do sistema desktop para plataforma web</li>
+                        <li>Desenvolvimento do sistema web com PHP, MsSql, Html, JavaScript, Ajax, XML, Jquery, Css e FPdf</li>
+                        <li>Desenvolvimento de um novo projeto com a utilização de ActionsScript, Flash/Flex, Postgres, MsSql e PHP</li>
+                        <li>Utiliza&ccedil;&atilde;o do Photoshop para o recorte, edi&ccedil;&atilde;o e cria&ccedil;&atilde;o de imagens</li>
+                    </ul>
+
+                    <br /><h4>Suporte t&eacute;cnico</h4>
+                    <p>Sponte (<a href="http://www.sponte.com.br">www.sponte.com.br</a>), Pato Branco, Paran&aacute;. (Dezembro, 2008 - Dezembro, 2009)</p>
+                    <ul>
+                        <li>Atendendimento direto ao cliente via chat, telefone e skype</li>
+                        <li>Elaboraçao de manuais de utiliza&ccedil;&atilde;o do sistema para o &uacute; final</li>
+                        <li>Realiza&ccedil;&atilde;o de testes no sistema a cada nova vers&atilde;o lan&ccedil;ada</li>
+                        <li>Realiza&ccedil;&atilde;o de treinamentos aos clientes via skype e VNC</li>
+                    </ul>
+
+                    <br /><h4>Garantia</h4>
+                    <p>FIAT - Fipal (<a href="http://www.fipal.com.br/concessionaria/pato-branco.html">www.fipal.com.br/concessionaria/pato-branco.html</a>), Pato Branco, Paran&aacute;. (Julho, 2009 - Novembro, 2009)</p>
+                    <ul>
+                        <li>Analise de pe&ccedil;as automotivas em busca de defeitos</li>
+                        <li>Comunica&ccedil;&atilde;o com a fabr&iacute;ca em caso de defeito nas pe&ccedil;as</li>
+                        <li>Codifica&ccedil;&atilde;o de defeitos de pe&ccedil;as e servi&ccedil;os</li>
+                        <li>Abertura e fechamento de ordens de servi&ccedil;o</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div id="conhecimentos">
+                <h2>
+                    Conhecimento em Tecnologias
+                </h2>
+
+                <div class="conhecimento">
+                    <h3>Banco de Dados:</h3>
+                    <ul>
+                        <li>Microsoft SQL Server 2000/2005/2008</li>
+                        <li>MySQL</li>
+                        <li>Postgres</li>
+                        <li>Access</li>
+                    </ul>
+
+                    <h3>Programa&ccedil;o desktop</h3>
+                    <ul>
+                        <li>Delphi 7/2007</li>
+                        <li>C++</li>
+                        <li>Pascal(FastReport)</li>
+                    </ul>
+
+                    <h3>Plataformas de Desenvolvimentos:</h3>
+                    <ul>
+                        <li>Windows</li>
+                        <li>Linux</li>
+                    </ul>
+                </div>
+
+                <div class="conhecimento">
+                    <h3>Programa&ccedil;o web (Front-end):</h3>
+                    <ul>
+                        <li>JavaScript/Jquery</li>
+                        <li>ActionScript</li>
+                        <li>HTML/XHTML - CSS</li>
+                        <li>Ajax</li>
+                    </ul>
+
+                    <h3>Programa&ccedil;o web (Back-end):</h3>
+                    <ul>
+                        <li>PHP</li>
+                        <li>Flash/Flex</li>
+                        <li>Ruby on Rails</li>
+                    </ul>
+
+                    <h3>Metodologias:</h3>
+                    <ul>
+                        <li>Extreme Programming (Desenvolvimento Iterativo, Refatoração)</li>
+                    </ul>
+                </div>
+                <br style="clear: both" />
+            </div>
+
+            <div id="formacao_academica">
+                <h2>
+                    Forma&ccedil;&atilde;o Acad&ecirc;mica
+                </h2>
+
+                <div>
+                    <h3>Ensino Superior:</h3>
+                    <ul>
+                        <li>Cursando 6º e &uacute;ltimo per&iacute;odo em An&aacute;lise e Desenvolvimento de Sistemas na UTFPR - Pato Branco</li>
+                    </ul>
+
+                    <h3>Ensino M&eacute;dio:</h3>
+                    <ul>
+                        <li>Conclu&iacute;do em dezembro de 2006 na UTFPR - Pato Branco</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div id="linguas_estrangeiras">
+                <h2>
+                    L&iacute;nguas estrangeiras
+                </h2>
+
+                <div>
+                    <ul>
+                        <li>Inlg&ecirc;s: Leitura (Avan&ccedil;ado), Escrita (Intermedi&aacute;rio), Conversa&ccedil;ão (Intermedi&aacute;rio)</li>
+                        <li>Espanhol: Leitura (Intermedi&aacute;rio), Escrita (B&aacute;sico), Conversa&ccedil;ão (B&aacute;sico)</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div id="cursos">
+                <h2>
+                    Cursos de aperfei&ccedil;oamento
+                </h2>
+
+                <div>
+                    <ul>
+                        <li>Curso Borland Delphi 2007 - Maio/2009 - 40 horas</li>
+                        <li>Gerenciamento de Projetos com Scrum - Palestra - 2 horas</li>
+                        <li>Minicurso Java/J2EE - Novembro/2006 - 8h</li>
+                        <li>Inform&aacute;tica Empresarial I e II - Janeiro/2005 - 120h</li
+                        <li>Ingl&ecirc;s - Mar&ccedil;o/2002 - 80h</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div id="rodape">
+                Thiago Cagnin Rodrigues - Pato Branco, Paran&aacute;
+            </div>
+
+        </div>
+    </body>
+</html>
